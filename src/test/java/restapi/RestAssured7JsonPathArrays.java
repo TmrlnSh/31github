3184aff8b32fab54bd455080ae62b1570e33e96a
Tamerlan <tamerlash11@gmail.com>
2023-12-08 18:40:04 +0000

    goooo

--- src/test/java/restapi/RestAssured7JsonPathArrays.java
+++ src/test/java/restapi/RestAssured7JsonPathArrays.java
@@ -39,7 +39,8 @@ public class RestAssured7JsonPathArrays {
         int thirdAccountNumber = userAccountsJsonPath.getInt("[2].accountNumber");
         System.out.println(thirdAccountNumber);
 
-        //fasfasfasfSDFDSgsdgsf
 
+        //lksafbjdv;kcsakdojfiahusldkvjid
+        //fasfasfasfSDFDSgsdgsfgi
     }
 }
