353769e4d705f94bc6b69df7ced8f892b0b1a9bd
Tamerlan <tamerlash11@gmail.com>
2023-12-17 23:51:20 +0600

    Test

---
+++ src/test/java/restapi/jackson/Test.java
@@ -0,0 +1,25 @@
+package restapi.jackson;
+
+public class Test {
+    public static void main(String[] args) {
+        //Написать функцию, которая будет возвращать true,
+        // если поданный на ввод массив int содержит 3 и
+        // более последовательных числа в любом месте.
+        Test test = new Test();
+        System.out.println(test.intSequence(new int[]{4, 3, 2,4,4,6}));
+    }
+    public boolean intSequence(int[] nums){
+        int count = 0;
+        for (int i =1; i < nums.length; i++){
+            if(nums[i]==nums[i-1]+1){
+                count++;
+                if (count>=3){
+                    return true;
+                }
+            }else {
+                count = 1;
+            }
+        }
+        return false;
+    }
+}
